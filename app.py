3e43c865ccb5afd66edc64adc10d8652a559e1a3
StarostinaVS <shel.v.99@gmail.com>
2024-03-12 20:41:02 +0900

    login register

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, session, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db, Customers
 from FDataBase import FDataBase
 import psycopg2 
 import psycopg2.extras
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required
+from werkzeug.utils import secure_filename
+from UserLogin import UserLogin
+import re 
 
 # dbase = FDataBase(db)
 
@@ -21,8 +26,6 @@ DB_PASS = "postgres"
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
-
-
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -30,7 +33,9 @@ migrate = Migrate(app, db)
   
 @app.route("/", methods=['GET','POST'])
 def index():
-
+  if 'loggedin' in session:
+        return render_template('index.html', username=session['username'])
+  
   cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
   if request.method == 'POST':
       fname=request.form['fname']
@@ -47,10 +52,11 @@ def index():
 
 #Страница недвижимость 
 
-@app.route("/realty")
+@app.route("/realty", methods=['GET','POST'])
 def realty():
+  
   cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-  s = 'SELECT * FROM public."Realty"'
+  s = f'SELECT * FROM public."Realty"'
   cur.execute(s)
   list_realty = cur.fetchall()
   return render_template('realty.html', title ='Недвижимость',\
@@ -79,6 +85,12 @@ def add_client():
       conn.commit()
       flash('Клиент успешно добавлен')
       return redirect(url_for('add_client'))
+  
+@app.route("/search", methods=['POST'])
+def search():
+  if request.method == 'POST':
+    object_type=request.form['object_type']
+    return redirect(url_for('add_client'))
 
 @app.route("/list_deal")
 def list_deal():
@@ -96,16 +108,78 @@ def add_realty():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login(): 
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST'and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cur.execute('SELECT * FROM  public."User" WHERE username = %s', (username,))
+        account = cur.fetchone()
+ 
+        if account:
+            password_rs = account['password']
+            print(password_rs)
+            if check_password_hash(password_rs, password):
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                return redirect(url_for('index'))
+            else:
+                flash('Неверное имя пользователя/пароль')
+        else:
+            flash('Неверное имя пользователя/пароль')
+ 
     return render_template('login.html', title="Вход")
 
+
 @app.route("/registration", methods=['POST', 'GET'])
-def registration(): 
+def registration():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+    
+        _hashed_password = generate_password_hash(password)
+ 
+        cursor.execute('SELECT * FROM public."User" WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            flash('Учетная запись уже существует!')
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('Имя пользователя должно содержать только латинские символы и цифры!')
+        elif not username or not password:
+            flash('Пожалуйста, заполните форму!')
+        elif  len(password) < 4:
+           flash('Ваш пароль слишком короткий!')
+        elif password != password2:
+           flash('Пароли не совпадают')
+        else:
+            cursor.execute('INSERT INTO public."User"( username, password) VALUES (%s,%s)', (username, _hashed_password))
+            conn.commit()
+            flash('Вы успешно зарегистрировались!')
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        flash('Пожалуйста, заполните форму!')
+
     return render_template('registration.html', title="Регистрация")
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('login'))
 
 @app.route("/add_user")
 def add_user():
    pass
    
+@app.route("/check_user", methods=['GET','POST'])
+def check_user():
+   pass
+
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Text, Date, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from datetime import date 
+import enum
 
 db=SQLAlchemy()
 
@@ -36,10 +37,6 @@ class Realty(db.Model):
       self.area=area
       self.num_rooms=num_rooms
 
-    def __repr__(self):
-        return f"Realty(object_type='{self.object_type}', location='{self.location}', " \
-               f"price='{self.price}', area={self.area}, num_rooms={self.num_rooms}, " \
-               f"description='{self.description}')"
 
 
 class Agents(db.Model):
@@ -48,7 +45,7 @@ class Agents(db.Model):
     fname = Column(String(40), nullable=False)
     lname = Column(String(40), nullable=False)
     email = Column(String(40))
-    phone = Column(Integer, nullable=False)
+    phone = Column(String(30), unique=True)
     salary = Column(Integer, nullable=False)
 
     def __init__(self,fname,lname,email,phone,salary):
@@ -58,60 +55,75 @@ class Agents(db.Model):
       self.phone=phone
       self.salary=salary
 
-    def __repr__(self):
-        return f"Agents(id={self.id}, fname='{self.fname}', lname='{self.lname}', " \
-               f"email='{self.email}', phone={self.phone}, salary={self.salary})"
 
 
-class Deal(db.Model):
-    __tablename__ = 'Deal'
+class Tickets(db.Model):
+    __tablename__ = 'Tickets'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('Customers.id'), nullable=False)
+    agent_id = Column(Integer, ForeignKey('Agents.id'), nullable=False)
+    tickets_start_date = Column(Date, nullable=False, default=date.today())
+
+    customer = relationship("Customers")
+    realty = relationship("Realty")
+
+    def __init__(self,customer_id,agent_id,tickets_start_date):
+      self.customer_id=customer_id
+      self.agent_id=agent_id
+      self.tickets_start_date=tickets_start_date
+
+class Operations(db.Model):
+    __tablename__ = 'Operations'
+    id = Column(Integer, primary_key=True)
+    operation_type = Column(String (20), nullable=False)
+
+    def __init__(self,operation_type):
+       self.operation_type=operation_type
+
+
+class Contract(db.Model):
+    __tablename__ = 'Contract'
     id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey('Customers.id'), nullable=False)
     realty_id = Column(Integer, ForeignKey('Realty.id'), nullable=False)
     agent_id = Column(Integer, ForeignKey('Agents.id'), nullable=False)
-    deal_date = Column(Date, nullable=False, default=date.today())
+    operations_id = Column(Integer, ForeignKey('Operations.id'), nullable=False)
+    contract_start_date = Column(Date, nullable=False, default=date.today())
+    contract_end_date = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
 
     customer = relationship("Customers")
     realty = relationship("Realty")
     agent = relationship("Agents")
 
-    def __init__(self,customer_id,realty_id,agent_id,deal_date,price):
+    def __init__(self,customer_id,realty_id,agent_id,operations_id,contract_start_date,contract_end_date,price):
       self.customer_id=customer_id
       self.realty_id=realty_id
       self.agent_id=agent_id
-      self.deal_date=deal_date
+      self.operations_id=operations_id
+      self.contract_start_date=contract_start_date
+      self.contract_end_date=contract_end_date
       self.price=price
 
-    def __repr__(self):
-        return f"Deal(id={self.id}, " \
-               f"customer_id={self.customer_id}, " \
-               f"realty_id={self.realty_id}, " \
-               f"agent_id={self.agent_id}, " \
-               f"deal_date='{self.deal_date}', " \
-               f"price={self.price})"
+class User(db.Model):
+   __tablename__ = 'User'
+   id = Column(Integer, primary_key=True)
+   username = Column(String(40), nullable=False)
+   password = Column(Text, nullable=False)
 
+   def __init__(self, username, password):
+      self.username=username
+      self.password=password
 
-class Reviews(db.Model):
-    __tablename__ = 'Reviews'
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('Customers.id'), nullable=False)
-    realty_id = Column(Integer, ForeignKey('Realty.id'), nullable=False)
-    rating = Column(Integer, CheckConstraint("num_rooms>0 and num_rooms<6"))
-    comment = Column(Text)
+class Profile(db.Model):
+  __tablename__ = 'Profile'
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+  agent_id = Column(Integer, ForeignKey('Agents.id'), nullable=False)
 
-    customer = relationship("Customers")
-    realty = relationship("Realty")
+  customer = relationship("Customers")
+  agent = relationship("Agents")
 
-    def __init__(self,customer_id,realty_id,rating,comment):
-      self.customer_id=customer_id
-      self.realty_id=realty_id
-      self.rating=rating
-      self.comment=comment
-
-    def __repr__(self):
-        return f"Reviews(id={self.id}, " \
-               f"customer_id={self.customer_id}, " \
-               f"realty_id={self.realty_id}, " \
-               f"rating={self.rating}, " \
-               f"comment='{self.comment}')"
+  def __init__(self,customer_id,agent_id):
+    self.customer_id=customer_id
+    self.agent_id=agent_id
